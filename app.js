5866324e7694e8bdcc45cb2563c2637616d9a99f
Kaylee Horner <kayleeahorner@gmail.com>
2020-11-22 00:38:23 -0600

    fixed file to render HTML properly

--- app.js
+++ app.js
@@ -9,17 +9,15 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const team = [];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
+function renderHTML() {
+    console.log("All team members have been added.")
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(team), "utf-8");
+}
 
 function managerQuestion() {
     return inquirer.prompt([
@@ -50,8 +48,9 @@ function managerQuestion() {
             answers.id,
             answers.email,
             answers.officeNumber,
-        )}
         )
+        team.push(manager);}
+    )
 }
 
 function addQuestions() {
@@ -68,11 +67,10 @@ function addQuestions() {
         } else if (answer.role === "Engineer") {
             return engineerQuestion()
         } else {
-            return console.log("All team members have been added.");
-            renderHTML();
+            return renderHTML();
+            
         }
     });
-
 }
 
 function internQuestion() {
@@ -96,20 +94,18 @@ function internQuestion() {
         type: 'input',
         message: 'Where does the intern go to school?',
         name: 'school',
-    }, 
-]
-
-)
-.then((answers) => {
-    const newIntern = new Intern(
-        answers.name,
-        answers.email,
-        answers.github,
-    )}
-    )
-.then((answers) => {
-    addQuestions()}
-    );
+    }])
+    .then((answers) => {
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.school,
+        )
+        team.push(newIntern);
+    }).then((answers) => {
+    addQuestions()
+    });
 }
    
 function engineerQuestion() {
@@ -134,30 +130,19 @@ function engineerQuestion() {
         message: "What is the engineer's GitHub?",
         name: 'github',
     }, 
-])
-.then((answers) => {
-    const newEngineer = new Engineer(
-        answers.name,
-        answers.email,
-        answers.github,
-    )}
-    ).then((answers) => {
-    addQuestions()}
+    ]).then((answers) => {
+        const newEngineer = new Engineer(
+            answers.name,
+            answers.email,
+            answers.github,
+        )
+        team.push(newEngineer);
+    }).then((answers) => {
+        addQuestions()}
     );
-}
-
-function renderHTML() {
-    const answers = render([manager, newEngineer, newIntern]);
-    if (fs.existsSync(OUTPUT_DIR)) {
-        fs.writeFileSync(outputPath, answers)
-    } else {
-        fs.mkdirSync(OUTPUT_DIR);
-        fs.writeFileSync(outputPath, answers)
-    }
 }
 
 // function to initialize program
 managerQuestion().then((answers) => {
-    addQuestions()
+    addQuestions();
 })
-
